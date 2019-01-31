0d235f2e4f2b6b006fd2cbea4068d8721ae9c19a
Kittycoon <40754816+Kittycoon@users.noreply.github.com>
2019-01-30 19:56:30 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,6 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-const prefix = "!";
 client.on('message', message => {
    if (message.content === '!app Jessica') {
     	message.reply({
@@ -47,7 +46,46 @@ client.on('message', message => {
     ]
   }
 });
-        
+   }
+   if (message.content === '!app meh') {
+    	message.reply({
+  
+  "embed": {
+    "title": "title",
+    "description": "t",
+    "color": 1976851,
+    "thumbnail": {
+      "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+    },
+    "image": {
+      "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+    },
+    "fields": [
+      {
+        "name": "🤔",
+        "value": "some of these properties have certain limits..."
+      },
+      {
+        "name": "😱",
+        "value": "try exceeding some of them!"
+      },
+      {
+        "name": "🙄",
+        "value": "an informative error should show up, and this view will remain as-is until all issues are fixed"
+      },
+      {
+        "name": "<:thonkang:219069250692841473>",
+        "value": "these last two",
+        "inline": true
+      },
+      {
+        "name": "<:thonkang:219069250692841473>",
+        "value": "are inline fields",
+        "inline": true
+      }
+    ]
+  }
+});
   	}
     if (message.content === '!app') {
     	message.reply('please specify the agent to follow up on.');
