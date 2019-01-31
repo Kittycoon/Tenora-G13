e508171aa21a8048109b87a2fa39c771731fb8de
Kittycoon <40754816+Kittycoon@users.noreply.github.com>
2019-01-30 19:46:43 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,47 +9,14 @@ client.on('ready', () => {
 const prefix = "!";
 client.on('message', message => {
    if (message.content === '!app Jessica') {
-    	message.reply({
-  "embed": {
-    "title": "_`STARS PROFILE PREVIEW`_",
-    "url": "https://discordapp.com",
-    "color": 0xffbf00,
-    "timestamp": "2019-01-31T01:43:56.510Z",
-    "thumbnail": {
-      "url": "https://cdn.discordapp.com/embed/avatars/0.png"
-    },
-    "image": {
-      "url": "https://cdn.discordapp.com/embed/avatars/0.png"
-    },
-    "fields": [
-      {
-        "name": "Name:",
-        "value": "`Jessica Aquila`"
-      },
-      {
-        "name": "Age:",
-        "value": "`25`"
-      },
-      {
-        "name": "Alignment:",
-        "value": "Lol I dont even fucken know which side is what this is just a demo anyways."
-      },
-      {
-        "name": "Primary Weapon:",
-        "value": "`Sniper Rifle`",
-        "inline": true
-      },
-      {
-        "name": "Weapon Mastery",
-        "value": "`good succ ability`",
-        "inline": true
-      }
-    ]
-  }
-});
+    	message.reply(`\n Name: Jessica Aquila \n Age: 25`);
+        
+  	}
     if (message.content === '!app') {
     	message.reply('please specify the agent to follow up on.');
   	}
 });
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
+
