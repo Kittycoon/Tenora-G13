3c633b6bb3a7624a90c093236eb7fe37b4a6bbb0
Kittycoon <40754816+Kittycoon@users.noreply.github.com>
2019-01-30 19:51:47 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,44 @@ client.on('ready', () => {
 const prefix = "!";
 client.on('message', message => {
    if (message.content === '!app Jessica') {
-    	message.reply(`\n Name: Jessica Aquila \n Age: 25`);
+    	message.reply({
+  
+  "embed": {
+    "title": "title",
+    "description": "t",
+    "color": 1976851,
+    "thumbnail": {
+      "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+    },
+    "image": {
+      "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+    },
+    "fields": [
+      {
+        "name": "🤔",
+        "value": "some of these properties have certain limits..."
+      },
+      {
+        "name": "😱",
+        "value": "try exceeding some of them!"
+      },
+      {
+        "name": "🙄",
+        "value": "an informative error should show up, and this view will remain as-is until all issues are fixed"
+      },
+      {
+        "name": "<:thonkang:219069250692841473>",
+        "value": "these last two",
+        "inline": true
+      },
+      {
+        "name": "<:thonkang:219069250692841473>",
+        "value": "are inline fields",
+        "inline": true
+      }
+    ]
+  }
+});
         
   	}
     if (message.content === '!app') {
